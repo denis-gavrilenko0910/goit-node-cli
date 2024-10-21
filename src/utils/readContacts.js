775e9dd5c25acbb8a9f49d7d81e0a62ad431ab8b
denis.g87 <denys.gavrylenko@itechhub.io>
2024-10-22 01:03:34 +0300

    added: final scripts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -3,5 +3,6 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const readContacts = async () => {
   const data = await readFile(PATH_DB, 'utf-8');
+  console.log(data);
   return JSON.parse(data);
 };
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,6 +1,5 @@
 import { writeFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {
+export const writeContacts = async (updatedContacts) =>
   writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
-};
